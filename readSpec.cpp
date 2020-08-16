f127426bc2e294e4d9faa92a980b9accf6378ef6
wangzhenhao1994 <wangzhh@outlook.com>
2020-08-17 00:06:27 +0200

    test

---
+++ readSpec.cpp
@@ -0,0 +1,18 @@
+/* Includes */
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "api/seabreezeapi/SeaBreezeAPI.h"
+
+#ifndef _WINDOWS
+#include <unistd.h>
+#else
+#include <Windows.h>
+#endif
+
+int api_test() {
+    int number_of_devices;
+    long *device_ids;
+    SeaBreezeAPI API;
+    return 0;
+  }
