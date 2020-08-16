122a61182dfadc47397cd9c8cebbc80f7e4457cd
wangzhenhao1994 <wangzhh@outlook.com>
2020-08-17 01:29:03 +0200

    Modify1

--- readSpec.cpp
+++ readSpec.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include "api/seabreezeapi/SeaBreezeAPI.h"
+#include "SeaBreeze/include/api/seabreezeapi/SeaBreezeAPI.h"
 
 #ifndef _WINDOWS
 #include <unistd.h>
@@ -10,9 +10,24 @@
 #include <Windows.h>
 #endif
 
-int api_test() {
-    int number_of_devices;
-    long *device_ids;
-    SeaBreezeAPI API;
-    return 0;
+using namespace std;
+
+int readSpec() {
+  SeaBreezeAPI::shutdown();
+  SeaBreezeAPI* API = SeaBreezeAPI::getInstance();
+  unsigned long number_of_devices;
+  long *device_ids;
+  long *device_id;
+  int *error_code=0;
+
+  if (API->probeDevices()){
+    cout<<"No spectrometer is detected!"<<endl;
   }
+  number_of_devices = API->getNumberOfDeviceIDs();
+  device_ids = (long *)calloc(number_of_devices, sizeof(long));
+  API->getDeviceIDs(device_ids, number_of_devices);
+  API->openDevice(device_ids[0], error_code);
+  API->spectrometerGetWavelengths(long deviceID, long spectrometerFeatureID, int *errorCode, double *wavelengths, int length);
+  return 0;
+}
+//
