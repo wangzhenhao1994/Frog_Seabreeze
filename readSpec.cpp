1d5e3da52327a8f201e146b952450b5110fa2be8
wangzhenhao1994 <wangzhh@outlook.com>
2020-08-17 16:32:56 +0200

    Test!

--- readSpec.cpp
+++ readSpec.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include "SeaBreeze/include/api/seabreezeapi/SeaBreezeAPI.h"
+#include "api/seabreezeapi/SeaBreezeAPI.h"
 
 #ifndef _WINDOWS
 #include <unistd.h>
@@ -15,19 +15,40 @@ using namespace std;
 int readSpec() {
   SeaBreezeAPI::shutdown();
   SeaBreezeAPI* API = SeaBreezeAPI::getInstance();
+
   unsigned long number_of_devices;
   long *device_ids;
-  long *device_id;
+  long device_id;
   int *error_code=0;
+  char *nameBuffer;
+  int flag;
+  long *buffer;
+  long *buffer_s;
+  long length;
 
-  if (API->probeDevices()){
-    cout<<"No spectrometer is detected!"<<endl;
-  }
+  API->probeDevices();
   number_of_devices = API->getNumberOfDeviceIDs();
+  cout<<number_of_devices<<endl;
   device_ids = (long *)calloc(number_of_devices, sizeof(long));
-  API->getDeviceIDs(device_ids, number_of_devices);
-  API->openDevice(device_ids[0], error_code);
-  API->spectrometerGetWavelengths(long deviceID, long spectrometerFeatureID, int *errorCode, double *wavelengths, int length);
+  number_of_devices=API->getDeviceIDs(device_ids, number_of_devices);
+
+  nameBuffer=(char *)calloc(80, sizeof(char));
+  flag=API->getDeviceType(device_id, error_code, nameBuffer, 79);
+  if(flag > 0) {
+      cout<<"The device type is: "<<nameBuffer<<endl;
+  }
+  int status=API->openDevice(device_id, error_code);
+
+  if (status) {
+    cout<<"Can't open this spectrometer!"<<endl;
+  }
+
+  length=API->getNumberOfRawUSBBusAccessFeatures(device_id, error_code);
+  cout<<"Length is: "<<length<<endl;
+  buffer_s=(long *)calloc(20, sizeof(long));
+  API->getRawUSBBusAccessFeatures(device_id, error_code, buffer_s, 1);
+  cout<<"beffer_s is  "<<buffer_s[0]<<endl;
+  //API->rawUSBBusAccessRead(device_id, 0, error_code, buffer, unsigned int bufferLength, unsigned char endpoint);
   return 0;
 }
 //
--- readSpec_cpp.d
+++ readSpec_cpp.d
@@ -1,7 +1,7 @@
 
 # DO NOT DELETE
 
-./readSpec_cpp.so: SeaBreeze/include/api/seabreezeapi/SeaBreezeAPI.h
-./readSpec_cpp.so: /mnt/c/Users/wangz/Desktop/Frog_Seabreeze/./SeaBreeze/include/api/DllDecl.h
+./readSpec_cpp.so: /home/wangzhh/Documents/github/Frog_Seabreeze/./SeaBreeze/include/api/seabreezeapi/SeaBreezeAPI.h
+./readSpec_cpp.so: /home/wangzhh/Documents/github/Frog_Seabreeze/./SeaBreeze/include/api/DllDecl.h
 ./readSpec_cpp.so: /home/wangzhh/Programs/ROOT/include/RVersion.h /home/wangzhh/Programs/ROOT/include/RConfig.h /home/wangzhh/Programs/ROOT/include/TClass.h /home/wangzhh/Programs/ROOT/include/TDictAttributeMap.h /home/wangzhh/Programs/ROOT/include/TInterpreter.h /home/wangzhh/Programs/ROOT/include/TROOT.h /home/wangzhh/Programs/ROOT/include/TBuffer.h /home/wangzhh/Programs/ROOT/include/TMemberInspector.h /home/wangzhh/Programs/ROOT/include/TError.h /home/wangzhh/Programs/ROOT/include/RtypesImp.h /home/wangzhh/Programs/ROOT/include/TIsAProxy.h /home/wangzhh/Programs/ROOT/include/TFileMergeInfo.h /home/wangzhh/Programs/ROOT/include/TCollectionProxyInfo.h /home/wangzhh/Programs/ROOT/bin/rootcling
-readSpec_cpp__ROOTBUILDVERSION= 6.22/01
+readSpec_cpp__ROOTBUILDVERSION= 6.23/01
