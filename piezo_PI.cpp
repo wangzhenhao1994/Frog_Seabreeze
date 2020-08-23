4b038ecab29674b78618570dec8670eb0b5e6e07
wangzhenhao1994 <wangzhh@outlook.com>
2020-08-23 11:33:16 +0200

    test

--- piezo_PI.cpp
+++ piezo_PI.cpp
@@ -10,7 +10,7 @@ public:
   PI_Stage(const char* id = "/dev/ttyUSB0");
   void piezo_initializer();
   int move_onestep(int ID, const char* szAxes, double* step_length);
-  void exit(int ID);
+  void exit();
 
 private:
   char* sz_buffer;
@@ -18,8 +18,11 @@ private:
   const char* dev_id;
   int controller_id=-1;
   const char* axes_id="1";
+  const double* pdLowvoltageArray;
   BOOL servo_mode=TRUE;
+  int atz_flag;
   int connection_flag=0;
+  int servo_flag=-1;
   int ont_flag=-1;
   BOOL ont_state=-1;
   double min_range;
@@ -31,7 +34,18 @@ PI_Stage::PI_Stage(const char* id = "/dev/ttyUSB0"):dev_id (id){}
 void PI_Stage::piezo_initializer(){
   controller_id=PI_ConnectRS232ByDevName(dev_id, 115200);
   connection_flag=PI_IsConnected(controller_id);
-  PI_SVO(controller_id, axes_id, &servo_mode);
+  atz_flag=PI_ATZ(controller_id, axes_id, pdLowvoltageArray, &servo_mode);
+  if (atz_flag){
+    cout<<"Auto zero Successfully!"<<endl;
+  }else{
+    cout<<"Something is wrong!"<<endl;
+  }
+  servo_flag=PI_SVO(controller_id, axes_id, &servo_mode);
+  if (servo_flag){
+    cout<<"Stage is in Closed-Loop mode!"<<endl;
+  }else{
+    cout<<"Something is wrong!"<<endl;
+  }
   PI_qTMN(controller_id, axes_id, &min_range);
   PI_qTMX(controller_id, axes_id, &max_range);
   PI_MOV(controller_id, axes_id, &min_range);
@@ -59,7 +73,7 @@ int PI_Stage::move_onestep(int ID, const char* szAxes, double* step_length){
   }
 }
 
-void PI_Stage::exit(int ID){
+void PI_Stage::exit(){
   PI_CloseConnection (controller_id);
   return;
 }
@@ -67,5 +81,6 @@ void PI_Stage::exit(int ID){
 int piezo_PI(){
   PI_Stage stage;
   stage.piezo_initializer();
+  stage.exit();
   return 0;
 }
