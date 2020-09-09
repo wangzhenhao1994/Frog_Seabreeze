83055994fc129cf5822da366de93ab307cf87e31
wangzhenhao1994 <wangzhh@outlook.com>
2020-09-09 08:50:21 -0700

    test

--- test.cpp
+++ test.cpp
@@ -1,42 +1,60 @@
+/***
+ * This example expects the serial port has a loopback on it.
+ *
+ * Alternatively, you could use an Arduino:
+ *
+ * <pre>
+ *  void setup() {
+ *    Serial.begin(<insert your baudrate here>);
+ *  }
+ *
+ *  void loop() {
+ *    if (Serial.available()) {
+ *      Serial.write(Serial.read());
+ *    }
+ *  }
+ * </pre>
+ */
 
+#include <string>
 #include <iostream>
-#include <boost/thread.hpp>
+#include <cstdio>
 
-#include "BufferedAsyncSerial.h"
+// OS Specific sleep
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
-using namespace std;
-using namespace boost;
+#include "serial/serial.h"
+
+using std::string;
+using std::exception;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+
+void my_sleep(unsigned long milliseconds) {
+#ifdef _WIN32
+      Sleep(milliseconds); // 100 ms
+#else
+      usleep(milliseconds*1000); // 100 ms
+#endif
+}
 
 int test()
 {
-	try {
-		cout << "Start\n";
+    serial::Serial my_serial("/dev/ttyUSB0", 9600, serial::Timeout::simpleTimeout(1000));
 
-		BufferedAsyncSerial serial("/dev/ttyUSB0", 9600);
-
-		//Return immediately. String is written *after* the function returns,
-		//in a separate thread.
-		//serial.writeString("Hello world\r\n");
-
-		//Simulate doing something else while the serial device replies.
-		//When the serial device replies, the second thread stores the received
-		//data in a buffer.
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-
-		//Always returns immediately. If the terminator \r\n has not yet
-		//arrived, returns an empty string.
-		cout << serial.readStringUntil("\r")<<endl;
-		cout << serial.readStringUntil("\r")<<endl;
-		cout << serial.readStringUntil("\r")<<endl;
-		cout << serial.readStringUntil("\r")<<endl;
-
-		serial.close();
-
-		cout << "End\n";
-	}
-	catch(boost::system::system_error& e)
-	{
-		cout<<"Error: "<<e.what()<<endl;
-		return 1;
-	}
+    cout << "Is the serial port open?";
+    if(my_serial.isOpen())
+        cout << " Yes." << endl;
+    else
+        cout << " No." << endl;
+    my_serial.write("rd\r");
+    string s=my_serial.read(100);
+    cout<<s;
+    return 0;
 }
