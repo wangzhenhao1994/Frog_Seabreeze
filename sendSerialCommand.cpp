adfe6706cbacadf68a1d7205d089c9ea84cc1dd2
wangzhenhao1994 <wangzhh@outlook.com>
2023-06-25 21:05:09 +0200

    cc

--- sendSerialCommand.cpp
+++ sendSerialCommand.cpp
@@ -1,58 +1,23 @@
-/**
- * @file /example1/main.cpp
- * @author Philippe Lucidarme
- * @date December 2019
- * @brief File containing example of serial port communication
- *
- * This example send the ASCII table through the serial device
- *
- * @see https://lucidar.me
- */
+#define DATA_LENGTH 255
 
+#include "SerialPort.hpp"
+#include "SerialPort.cpp"
+#include <iostream>
 
-// Serial library
-#include "serialib.h"
-#include <windows.h>
-#include <stdio.h>
+const char *portName = "\\\\.\\COM20";
 
+//Declare a global object
+SerialPort *arduino;
 
-#if defined (_WIN32) || defined(_WIN64)
-    #define SERIAL_PORT "COM1"
-#endif
-#ifdef __linux__
-    #define SERIAL_PORT "/dev/ttyACM0"
-#endif
+//Here '\n' is a delimiter 
+const char *sendString = "Hello World\n"; 
 
-
-/*!
- * \brief main  Simple example that send ASCII characters to the serial device
- * \return      0 : success
- *              <0 : an error occured
- */
-int main( /*int argc, char *argv[]*/)
+void sendSerialCommand()
 {
-    // Serial object
-    serialib serial;
-
-
-    // Connection to serial port
-    char errorOpening = serial.openDevice(SERIAL_PORT, 115200);
-
-
-    // If connection fails, return the error code otherwise, display a success message
-    if (errorOpening!=1) return errorOpening;
-    printf ("Successful connection to %s\n",SERIAL_PORT);
-
-
-    // Display ASCII characters (from 32 to 128)
-    for (int c=32;c<128;c++)
-    {
-        serial.writeChar(c);
-        usleep(10000);
-    }
-
-    // Close the serial device
-    serial.closeDevice();
-
-    return 0 ;
+  arduino = new  SerialPort(portName);
+  if (arduino->isConnected()){
+    bool hasWritten = arduino->writeSerialPort(sendString, DATA_LENGTH);
+    if (hasWritten) std::cout << "Data Written Successfully" << std::endl;
+    else std::cerr << "Data was not written" << std::endl;
+  }
 }
